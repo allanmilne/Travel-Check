dd9ff2eaacc22c576b6717abc93784d7beedb0d8
Allan Milne <amilne.wd@gmail.com>
2019-08-25 19:31:26 +0100

    adds trip CRUD

--- db/seed.rb
+++ db/seed.rb
@@ -3,9 +3,9 @@ require_relative( "../models/city.rb" )
 require_relative( "../models/trip.rb" )
 require("pry-byebug")
 
+Trip.delete_all()
 City.delete_all()
 Country.delete_all()
-# Trip.delete_all()
 
 country1 = Country.new({"name" => "China",})
 country1.save()
@@ -16,9 +16,15 @@ city1 = City.new({"name" => "Shanghai", "country_id" => country1.id})
 city1.save
 city2 = City.new({"name" => "Beijing", "country_id" => country1.id})
 city2.save
-
 city3 = City.new({"name" => "Rio de Janeiro", "country_id" => country2.id})
 city3.save
+
+trip1 = Trip.new({"city_id" => city2.id, "visited" => true})
+trip1.save
+trip2 = Trip.new({"city_id" => city3.id, "visited" => false})
+trip2.save
+trip3 = Trip.new({"city_id" => city1.id, "visited" => false})
+trip3.save
 
 binding.pry
 nil
